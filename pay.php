e17506a3f275a6cfe3e0623d32f091294790b5d0
kumarself10 <55672982+kumarself10@users.noreply.github.com>
2020-07-27 13:34:46 +0530

    Add files via upload

--- pay.php
+++ pay.php
@@ -5,7 +5,7 @@ $name = $_POST["name"];
 $phone = $_POST["phone"];
 $email = $_POST["email"];
 include 'instamojo.php';
-$api = new Instamojo\Instamojo('6dcdc7a627a360c9483f6d01f91d9c8a', '74161df4b28ba8583eb37942d1b0ce89','https://test.instamojo.com/api/1.1/');
+$api = new Instamojo\Instamojo('6dcdc7a627a360c9483f6d01f91d9c8a', '74161df4b28ba8583eb37942d1b0ce89','https://www.instamojo.com/api/1.1/');
 try {
     $response = $api->paymentRequestCreate(array(
         "purpose" => $purpose,
