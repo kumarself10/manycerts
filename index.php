f461261f874f591067596a8beff217b77283437e
kumarself10 <55672982+kumarself10@users.noreply.github.com>
2020-07-27 12:11:55 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,45 +1,51 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-	<title>Thank You, Mojo</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  </head>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="../../favicon.ico">
+		<title>Payment Mojo</title>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
 	<body>
     <div class="container">
 	<div class="page-header">
         <h1><a href="index.php">Instamojo Payment</a></h1>
-        <p class="lead">A test payment integration for instamojo paypemnt gateway. Written in PHP</p>
-    </div>
-	<h3 style="color:#6da552">Thank You, Payment succus!!</h3>
-<?php
-include 'src/instamojo.php';
-$api = new Instamojo\Instamojo('YOU_PRIVATE_API_KEY', 'YOUR_PRIVATE_AUTH_TOKEN','https://test.instamojo.com/api/1.1/');
-$payid = $_GET["payment_request_id"];
-try {
-    $response = $api->paymentRequestStatus($payid);
-	echo "<h4>Payment ID: " . $response['payments'][0]['payment_id'] . "</h4>" ;
-    echo "<h4>Payment Name: " . $response['payments'][0]['buyer_name'] . "</h4>" ;
-    echo "<h4>Payment Email: " . $response['payments'][0]['email'] . "</h4>" ;
-echo "<pre>";
-   print_r($response);
-echo "</pre>";
-}
-catch (Exception $e) {
-    print('Error: ' . $e->getMessage());
-}
-?>
-</div> <!-- /container -->
-</body>
+		<form action="pay.php" method="POST" accept-charset="utf-8">
+			<input type="hidden" name="product_name" value="<?php echo $prd_name; ?>"> 
+			<input type="hidden" name="product_price" value="<?php echo $prd_price; ?>"> 
+			<div class="form-group">
+			<label>Your Name</label>
+			<input type="text" class="form-control" name="name" placeholder="Enter your name">	 
+			</div>
+			<div class="form-group">
+			<label>Your Phone</label>
+			<input type="text" class="form-control" name="phone" placeholder="Enter your phone number"> 
+			</div>
+			<div class="form-group">
+			<label>Your Email</label>
+			<input type="email" class="form-control" name="email" placeholder="Enter you email"> 
+			</div>
+			<div class="form-group">
+			<label>Amount</label>
+			<input type="email" class="form-control" name="amount" Value="100" readonly>
+			</div>
+			<p><input type="submit" class="btn btn-success btn-lg" value="Click here to Pay"></p>
+		</form>
+ 
+    
+    </div> <!-- /container -->
+		</div>
+
+
+  </body>
 </html>
