7c51876d0e33b67cc569cdfd2d9bb26cec47fc5a
kumarself10 <55672982+kumarself10@users.noreply.github.com>
2020-07-27 13:37:46 +0530

    Add files via upload

--- pay.php
+++ pay.php
@@ -4,7 +4,7 @@ $amount = $_POST["amount"];
 $name = $_POST["name"];
 $phone = $_POST["phone"];
 $email = $_POST["email"];
-include 'instamojo.php';
+include 'src/instamojo.php';
 $api = new Instamojo\Instamojo('6dcdc7a627a360c9483f6d01f91d9c8a', '74161df4b28ba8583eb37942d1b0ce89','https://www.instamojo.com/api/1.1/');
 try {
     $response = $api->paymentRequestCreate(array(
@@ -16,8 +16,8 @@ try {
         "send_email" => true,
         "send_sms" => true,
         'allow_repeated_payments' => false,
-        "redirect_url" => "https://manycerts.com/thankyou.php",
-        "webhook" => "https://manycerts.com/webhook.php"
+        "redirect_url" => "https://studentstutorial.com/instamojo/thankyou.php",
+        "webhook" => "https://studentstutorial.com/instamojo/webhook.php"
         ));
    $pay_ulr = $response['longurl'];
     header("Location: $pay_ulr");
@@ -27,4 +27,3 @@ catch (Exception $e) {
     print('Error: ' . $e->getMessage());
 }     
  ?>
- 
