528aa6981ce18cfb6fe1815513bf82969ca22cd1
kumarself10 <55672982+kumarself10@users.noreply.github.com>
2020-07-31 12:12:57 +0530

    Add files via upload

---
+++ CISSP.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="../../favicon.ico">
+		<title>ManyCerts Payment</title>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+    <div class="container">
+	<div class="page-header">
+        <h1><a href="index.php">CISSP Videos - Material - Exams</a></h1>
+		<form action="pay.php" method="POST" accept-charset="utf-8">
+			<h2>
+			1. CISSP Offical ISC2 videos worth 560$ </br>
+			2. CISSP Books worth 200$ plus</br>
+			3. Certification Questions worth 100$</br>
+			4. All for 7000 rs
+			</br>
+			
+			</br>
+			
+			</h2>
+           
+			<a href="https://www.instamojo.com/@manycerts/l820fcdcee8bd426db33db811e732e4e9/" rel="im-checkout" data-text="Click to Pay" data-css-style="color:#ffffff; background:#008b02; width:300px; border-radius:4px"   data-layout="vertical"></a>
+			<script src="https://js.instamojo.com/v1/button.js"></script>
+           	</form>
+ 
+    
+    </div> <!-- /container -->
+		</div>
+
+
+  </body>
+</html>
