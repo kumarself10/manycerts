3e20c99dbfa6bec39e36a5fb0cd6b526b62941c5
kumarself10 <55672982+kumarself10@users.noreply.github.com>
2020-07-31 11:42:02 +0530

    Add files via upload

---
+++ Python1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="../../favicon.ico">
+		<title>ManyCerts Payment</title>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+    <div class="container">
+	<div class="page-header">
+        <h1><a href="index.php">Python Training Videos By Real Time Expert - 25+ hours - 250 Rs Only</a></h1>
+		<form action="pay.php" method="POST" accept-charset="utf-8">
+
+           
+			<a href="https://www.instamojo.com/@manycerts/leb141c9e5cdc4fc29d6fbf61f2375b63/" rel="im-checkout" data-text="Pay" data-css-style="color:#ffffff; background:#008b02; width:300px; border-radius:4px"   data-layout="vertical"></a>
+			<script src="https://js.instamojo.com/v1/button.js"></script>
+           	</form>
+ 
+    
+    </div> <!-- /container -->
+		</div>
+
+
+  </body>
+</html>
