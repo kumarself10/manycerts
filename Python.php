2bc9effeb99af083fddd365de4dc70a687590679
kumarself10 <55672982+kumarself10@users.noreply.github.com>
2020-07-31 11:34:30 +0530

    Add files via upload

--- Python.php
+++ Python.php
@@ -45,7 +45,7 @@
             		
 			</div>
             <label>Amount</label>
-			<a href="https://www.instamojo.com/@manycerts/l81e87f7ec29e43caaf50dd511fe46398/" rel="im-checkout" data-text="Pay" data-css-style="color:#ffffff; background:#008b02; width:300px; border-radius:4px"   data-layout="vertical"></a>
+			<<a href="https://www.instamojo.com/@manycerts/leb141c9e5cdc4fc29d6fbf61f2375b63/" rel="im-checkout" data-text="Pay" data-css-style="color:#ffffff; background:#008b02; width:300px; border-radius:4px"   data-layout="vertical"></a>
 			<script src="https://js.instamojo.com/v1/button.js"></script>
            
 		</form>
